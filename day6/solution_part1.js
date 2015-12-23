6d806cad3c1e6f85af206db890034d88762a8c8a
Eduardo Shiota Yasuda <yl.edu.sy@gmail.com>
2015-12-23 23:44:50 +0100

    Day 6 /o/

---
+++ day6/solution_part1.js
@@ -0,0 +1,51 @@
+function turnLightRangesOnOrOff(grid, initRange, endRange, operation) {
+    for (var i = initRange[0]; i <= endRange[0]; i++) {
+        for (var j = initRange[1]; j <= endRange[1]; j++) {
+            switch(operation) {
+                case 'turn on':
+                    grid[i][j] = 1;
+                    break;
+                case 'turn off':
+                    grid[i][j] = 0;
+                    break;
+                default:
+                    grid[i][j] = grid[i][j] === 0 ? 1 : 0;
+            }
+        }
+    }
+}
+
+function countOnLights(grid) {
+    return grid.reduce(function(count, column) {
+        return column.reduce(function(innerCount, light) {
+            if (light === 1) {
+                return ++innerCount;
+            }
+
+            return innerCount
+        }, count);
+    }, 0);
+}
+
+module.exports = function(input) {
+    var grid = [];
+
+    for (var i = 0; i < 1000; i++) {
+        grid[i] = [];
+
+        for (var j = 0; j < 1000; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    input.split(/\n/).forEach(function(instruction) {
+        var action = instruction.match(/^[a-z\s]+/)[0].trim();
+        var range = instruction.substr(action.length + 1).split(' through ');
+        var initRange = range[0].split(',').map(function(i) { return parseInt(i, 10); });
+        var endRange = range[1].split(',').map(function(i) { return parseInt(i, 10); });
+
+        turnLightRangesOnOrOff(grid, initRange, endRange, action);
+    });
+
+    return countOnLights(grid);
+};
